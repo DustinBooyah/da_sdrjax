c4964fdf3c2e3608f9792165c9482fd2ebefc020
Dustin Aguilar <dustin@dustinaguilar.com>
2015-04-29 16:49:19 -0400

    Updated Info, removed iframe in Contact section
    This first push I optimized the images, updated the info, removed the
    form and got her up on github

---
+++ free_settings.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$email_to = "asamedayapplianceservice@gmail.com"; // your email address
+
+$email_subject = "Jacksonville Repair Inquiry from Website"; // email subject line
+
+$thankyou = "thankyou.htm"; // thank you page
+
+
+
+?>
